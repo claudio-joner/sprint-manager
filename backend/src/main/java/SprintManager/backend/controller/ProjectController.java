bb00058f05023b89c47c38fb1f4a75af726e5a85
Claudio Escritorio <orochi256@gmail.com>
2026-07-11 11:28:19 -0300

    feat: agregar ejercicio MVC en memoria con Project

--- backend/src/main/java/SprintManager/backend/controller/ProjectController.java
+++ backend/src/main/java/SprintManager/backend/controller/ProjectController.java
@@ -1,10 +1,39 @@
 package SprintManager.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import SprintManager.backend.model.Project;
+import SprintManager.backend.service.ProjectService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectController {
-    
+    private ProjectService projectService= new ProjectService();
+
+    @GetMapping
+    public List<Project> getAll(){
+        return  projectService.getAll();
+    };
+
+    @GetMapping("/id")
+    public Project getById(@PathVariable Long id){
+        return projectService.getById(id);
+    };
+
+    @PostMapping
+    public Project create(@RequestBody Project project){
+        return projectService.create(project);
+    }
+
+    @DeleteMapping("/id")
+    public String delete(@PathVariable Long id){
+      boolean deleted = projectService.delete(id);
+      return deleted ? "Proyecto eliminado" : "Proyecto no encontrado";
+    };
+
+
+
+
+
 }
